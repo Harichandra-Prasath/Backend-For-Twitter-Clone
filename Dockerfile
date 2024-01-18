9208905fce1172a9001f169cfead04eae665dbf9
Harichandra-Prasath <1hcp302004@gmail.com>
2024-01-18 17:36:34 +0530

    Docker fixes

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1 
 
 WORKDIR /app
-
+    
 
 RUN pip install --upgrade pip
 
@@ -12,12 +12,11 @@ COPY ./requirements.txt /app/requirements.txt
 
 RUN pip install -r requirements.txt
 
-ADD . /app
-
 COPY . /app
 
 COPY ./entrypoint.sh /app/entrypoint.sh
 
-ENTRYPOINT [ "sh" , "/app/entrypoint.sh" ]
+ENTRYPOINT [ "sh", "/app/entrypoint.sh" ]
+
 
 
--- twitter/settings.py
+++ twitter/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+DEBUG =True
 
 ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1']
 
@@ -105,17 +105,6 @@ WSGI_APPLICATION = 'twitter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-"""    DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.postgresql_psycopg2',
-                'NAME': 'twitter',
-                'USER': 'postgres',
-                'PASSWORD': 'postgres123',
-                'HOST': 'localhost',
-                'PORT': 5432,
-            }
-        }
-"""
 
 
 DATABASES = {
